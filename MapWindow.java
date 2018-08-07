1dd6500f285329000d61ad6e56ddf0f181abda2e
Kossai <k1764181@kcl.ac.uk>
2018-08-07 22:57:09 +0200

    Added functionalities in the mapWindow class : the program can now for any given price range and borough of London , list and display the properties located in the borough which price is within the range; all of this displayed in a new window when a button is pressed.  Needs now to take into account the prices entered by the user. Changed the map of London as well.

--- MapWindow.java
+++ MapWindow.java
@@ -1,19 +1,91 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
-public class MapWindow extends Windows{
 
-    public MapWindow()
-    {
-      displayMap();
+public class MapWindow extends Windows {
+
+    public MapWindow() {
+        displayMap();
     }
 
     public void displayMap()
-    {
 
+    {
+        JPanel mapPanel = new JPanel();
         ImageIcon image = new ImageIcon("london-borough-map.jpg");
         JLabel imageLabel = new JLabel(image);
-        add(imageLabel);
+        mapPanel.add(imageLabel);
+        add(mapPanel);
+        map(0,100);
 
     }
 
+
+    public void map(int fromPrice, int toPrice)
+    {
+        JFrame f = new JFrame();
+        JPanel c = new JPanel();
+        for(String borough : BOROUGHS)
+        {
+            JButton b = new JButton("b");
+            ArrayList<String> l = listOfProperties(borough,fromPrice,toPrice );
+            b.addActionListener(e->displayProperties(l));
+            c.add(b);
+        }
+        f.getContentPane().add(c);
+        f.setVisible(true);
+        f.pack();
+    }
+
+
+    public int numberOfProperties(String borough, int fromPrice, int toPrice) {
+        int counter = 0;
+        for (AirbnbListing property : database) {
+            int price = property.getPrice();
+            String b = property.getNeighbourhood();
+
+            if ((b.equals(borough)) && (price <= toPrice) && (price >= fromPrice)) {
+                counter++;
+
+            }
+        }
+        return counter;
+
+    }
+
+    public ArrayList<String> listOfProperties(String borough, int fromPrice, int toPrice)
+    {
+        ArrayList<String> matchingProperties = new ArrayList<>();
+        for(AirbnbListing p : database)
+        {
+            int price = p.getPrice();
+            String b = p.getNeighbourhood();
+
+            if ((b.equals(borough)) && (price <= toPrice) && (price >= fromPrice))
+            {
+             matchingProperties.add(p.getHost_id());
+
+            }
+        }
+        return matchingProperties;
+    }
+
+    public void displayProperties(ArrayList<String> properties)
+    {
+     JFrame frame = new JFrame();
+     JPanel panel = new JPanel(new GridLayout(properties.size(),1,0,10));
+     for (String l : properties)
+     {
+      JLabel label = new JLabel("ID " + l);
+      panel.add(label);
+     }
+     JScrollPane sp = new JScrollPane(panel);
+     frame.getContentPane().add(sp);
+     frame.setVisible(true);
+     frame.pack();
+    }
+
 }
+
+
--- Windows.java
+++ Windows.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class Windows extends JPanel{
 
     protected final ArrayList<AirbnbListing> database;
+    protected static final String[] BOROUGHS  = {"Kingston upon Thames","Croydon","Bronley","Hounslow","Ealing","Havering",
+            "Hillingdon","Harrow","Brent","Barnet", "Enfield", "Waltham Forest", "Redbridge", "Sutton", "Lambeth", "Southwark",
+            "Lewisham", "Greenwich", "Bexley", "Richmond upon Thames", "Merton", "Wandsworth","Hammersmith and Fulham",
+            "Kensington and Chelsea","City of London","Westminster","Camden","Tower Hamlets", "Islington", "Hackney","Haringey",
+            "Newham","Barking and Dagenham"};
 
     public Windows()
     {
